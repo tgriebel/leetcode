50f43a7d4530c5a062ffe2f265e5cbe921ad99cb
Thomas Griebel <thomas.griebel@live.com>
2022-11-29 08:58:32 -0800

    Create 1020.cpp

---
+++ 1020.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int visit(vector<vector<int>>& grid, vector<vector<int>>& marked, int sr, int sc) {
+        if( sr >= grid.size() ) {
+            return -1;
+        }
+        if( sc >= grid[sr].size() ) {
+            return -1;
+        }
+        if( ( grid[sr][sc] == 0 ) || marked[sr][sc] ) {
+            return 0;
+        }
+        marked[sr][sc] = 1;
+        const int rightCell = visit( grid, marked, sr + 1, sc + 0 );
+        const int leftCell = visit( grid, marked, sr - 1, sc + 0 );
+        const int upperCell = visit( grid, marked, sr + 0, sc + 1 );
+        const int lowerCell = visit( grid, marked, sr + 0, sc - 1 );
+        if( ( rightCell < 0 ) || ( leftCell < 0 ) || ( upperCell < 0 ) || ( lowerCell < 0 ) ) {
+            return -1;
+        }
+        return ( 1 + rightCell + leftCell + upperCell + lowerCell );
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        vector<vector<int>> marked;
+        marked.resize(grid.size());
+        for( int r = 0; r < grid.size(); ++r ) {
+            marked[r].resize(grid[r].size());
+        }
+        for( int r = 0; r < grid.size(); ++r ) {
+            for( int c = 0; c < grid[r].size(); ++c ) {
+                marked[r][c] = 0;
+            }
+        }
+        int enclavesCount = 0;
+        for( int r = 0; r < grid.size(); ++r ) {
+            for( int c = 0; c < grid[r].size(); ++c ) {
+                const int count = visit( grid, marked, r, c );
+                if( count >= 1 ) {
+                    enclavesCount += count;
+                }
+            }
+        }
+        return enclavesCount;
+    }
+};
