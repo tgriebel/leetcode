d9b66df78dbb45bd35d185bdb5946abdc9524a62
Thomas Griebel <thomas.griebel@live.com>
2022-12-02 13:04:13 -0800

    Update 1523 - Count Odd Numbers in an Interval Range.cpp

--- 1523 - Count Odd Numbers in an Interval Range.cpp
+++ 1523 - Count Odd Numbers in an Interval Range.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int countOdds(int low, int high) {
         high = ( high - 1 ) | 0x01;
-        low |= 0x01;
+        low = low | 0x01;
         return ( ( high - low ) / 2 ) + 1;
     }
 };
