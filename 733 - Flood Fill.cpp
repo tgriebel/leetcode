c8d09050acfca70c7d9a92900c545ee953c36279
Thomas Griebel <thomas.griebel@live.com>
2022-12-05 08:18:09 -0800

    Update 733 - Flood Fill.cpp

--- 733 - Flood Fill.cpp
+++ 733 - Flood Fill.cpp
@@ -16,20 +16,19 @@ public:
         return true;
     }
     void fill(vector<vector<int>>& image, int sr, int sc, int targetColor, int replaceColor) {
-        if( sr >= image.size() ) {
-            return;
-        }
-        if( sc >= image[sr].size() ) {
-            return;
-        }
-        if( ( image[sr][sc] != targetColor ) || ( image[sr][sc] == replaceColor ) ) {
-            return;
-        }
         image[sr][sc] = replaceColor;
-        fill( image, sr + 1, sc + 0, targetColor, replaceColor );
-        fill( image, sr - 1, sc + 0, targetColor, replaceColor );
-        fill( image, sr + 0, sc + 1, targetColor, replaceColor );
-        fill( image, sr + 0, sc - 1, targetColor, replaceColor );
+        if( isValid( image, sr + 1, sc + 0, targetColor, replaceColor ) ) {
+            fill( image, sr + 1, sc + 0, targetColor, replaceColor );
+        }
+        if( isValid( image, sr - 1, sc + 0, targetColor, replaceColor ) ) {
+            fill( image, sr - 1, sc + 0, targetColor, replaceColor );
+        }
+        if( isValid( image, sr + 0, sc + 1, targetColor, replaceColor ) ) {
+            fill( image, sr + 0, sc + 1, targetColor, replaceColor );
+        }
+        if( isValid( image, sr + 0, sc - 1, targetColor, replaceColor ) ) {
+            fill( image, sr + 0, sc - 1, targetColor, replaceColor );
+        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>> filledImage = image;
