245c783ec3771f76e1f6d701d6b364ef2b010304
Thomas Griebel <thomas.griebel@live.com>
2022-12-05 18:45:34 -0600

    Update 1232 - Check If It Is a Straight Line.cpp

--- 1232 - Check If It Is a Straight Line.cpp
+++ 1232 - Check If It Is a Straight Line.cpp
@@ -1,35 +1,21 @@
 class Solution {
 public:
-    inline float getSlope( int x0, int y0, int x1, int y1 ) {
-        const float rise = ( y1 - y0 );
-        const float run = ( x1 - x0 );
-        if( run != 0.0f ) {
-            return ( rise / run );
-        } else {
-            return INT_MAX;
-        }
+    inline float getSlope( int dx, int dy ) {
+        return ( dx != 0 ) ? ( dy / static_cast<float>( dx ) ) : 1000000.0f;
     }
     bool checkStraightLine(vector<vector<int>>& coordinates) {
-        int x0 = coordinates[0][0];
-        int y0 = coordinates[0][1];
-        int x1 = coordinates[1][0];
-        int y1 = coordinates[1][1];
-        float m0 = getSlope( x0, y0, x1, y1 );
-        float m1 = 1.0f;
-
-        x0 = x1;
-        y0 = y1;
+        int dx = ( coordinates[1][0] - coordinates[0][0] );
+        int dy = ( coordinates[1][1] - coordinates[0][1] );
+        float m0 = getSlope( dx, dy );
 
         for( int i = 2; i < coordinates.size(); ++i ) {
-            x1 = coordinates[i][0];
-            y1 = coordinates[i][1];
+            dx = ( coordinates[i][0] - coordinates[i - 1][0] );
+            dy = ( coordinates[i][1] - coordinates[i - 1][1] );
+            const float m1 = getSlope( dx, dy );
 
-            m1 = getSlope( x0, y0, x1, y1 );
             if( ( m1 - m0 ) != 0.0f ) {
                 return false;
             }
-            x0 = x1;
-            y0 = y1;
             m0 = m1;
         }
         return true;
