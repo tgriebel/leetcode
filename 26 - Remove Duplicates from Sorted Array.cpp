49a3764a71a113be9ed088978f7033804d348cbd
Thomas Griebel <thomas.griebel@live.com>
2024-08-19 17:14:20 -0500

    Update 26 - Remove Duplicates from Sorted Array.cpp

--- 26 - Remove Duplicates from Sorted Array.cpp
+++ 26 - Remove Duplicates from Sorted Array.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         int lastUnique = 0;
-        for( int i = 1; i < nums.size(); ++i ) {
+        const int size = static_cast<int>( nums.size() );
+
+        for( int i = 1; i < size; ++i ) {
             if( nums[ i ] != nums[ lastUnique ] ) {
                 nums[ lastUnique + 1 ] = nums[ i ];
                 lastUnique++;
