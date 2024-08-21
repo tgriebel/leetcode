8f20062013a64738d0e4f33e05bbabae7e689620
Thomas Griebel <thomas.griebel@live.com>
2024-08-21 13:05:05 -0500

    Update 36 - Valid Sudoku.cpp

--- 36 - Valid Sudoku.cpp
+++ 36 - Valid Sudoku.cpp
@@ -1,10 +1,10 @@
 class Solution {
-    static const int bitMask = ~0x3; // Ascii values are assigned a bit, starts at '.' in ascii for speed
+    static const int bitMask = ~0x3; // Each ascii value is assigned a bit, starts at '.' in ascii for speed. Non-numbers are masked from checks
     static const int numRows = 9;
     static const int numCols = 9;
     static const int subBoxSize = 3;
 
-    inline static int getNum( const char pieceCode ) {
+    inline static int getBitNum( const char pieceCode ) {
         const int result = pieceCode - '.'; // Ascii: . / 0 1 2 ... 9
         return result;
     }
@@ -19,24 +19,30 @@ class Solution {
 
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        // For each row, column, and subGrid, check if a number was seen before. It's invalid if it was seen
+        // Use bitfields, for every possible check, to track if a number was seen 
+
         uint32_t rowCheck[ numRows ];
         uint32_t colCheck[ numCols ];
         uint32_t gridCheck[ subBoxSize * subBoxSize ];
 
+        // Process in row-order for better caching
         for( int j = 0; j < numRows; ++j ) {
             for( int i = 0; i < numCols; ++i ) {
-                const int num = getNum( board[ j ][ i ] );
+                const int bitNum = getBitNum( board[ j ][ i ] );
 
-                const int gridId = ( i / subBoxSize ) + subBoxSize * ( j / subBoxSize );
-
-                // Rows will finish before grids, grids before columns
-                const bool valid = isValid( rowCheck[ j ], num ) && isValid( gridCheck[ gridId ], num ) && isValid( colCheck[ i ], num );
-                if( valid == false ) {
+                const int gridId = ( i / subBoxSize ) + subBoxSize * ( j / subBoxSize ); // Requires integer division. Can't factor
+                    
+                // Short-circuit. Rows will finish before grids, grids before columns
+                // Branch prediction should not expect this condition. When we *do* hit it, the program is done
+                const bool valid = isValid( rowCheck[ j ], bitNum  ) && isValid( gridCheck[ gridId ], bitNum ) && isValid( colCheck[ i ], bitNum );
+                if( !valid ) {
                     return false;
                 }
-                markCell( rowCheck[ j ], num );
-                markCell( colCheck[ i ], num );
-                markCell( gridCheck[ gridId ], num );
+                // Mark numbers as seen
+                markCell( rowCheck[ j ], bitNum );
+                markCell( colCheck[ i ], bitNum );
+                markCell( gridCheck[ gridId ], bitNum );
             }
         }
         return true;
