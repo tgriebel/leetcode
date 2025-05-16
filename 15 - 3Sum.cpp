2492642983d1968aeac99ff1e7f865207b45f691
Thomas Griebel <thomas.griebel@live.com>
2025-05-16 13:37:33 -0700

    Update 15 - 3Sum.cpp

--- 15 - 3Sum.cpp
+++ 15 - 3Sum.cpp
@@ -1,26 +1,57 @@
 class Solution {
 public:
+
+    inline int binarySearch( vector<int>& nums, int low, int high, int x )
+    {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == x) {
+                return mid;
+            }
+
+            if (nums[mid] < x) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    inline int scanToUniqueValue( vector<int>& nums, int i ) {
+        int j = i + 1;
+        while( j < nums.size() && nums[ i ] == nums[ j ] ) {
+            ++j;
+        }
+        return j;
+    }
+
     vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        const int numCount = nums.size();
+        
+        std::sort(nums.begin(), nums.end());
+
         vector<vector<int>> result;
+        result.reserve( numCount );
 
-        // sort array so we know numbers are monotonic
-        sort( nums.begin(), nums.end() );
-
-        // scan to line between positives/negatives 
         int i = 0;
-        while( nums[ i ] < 0 ) {
-            ++i;
+
+        while( i < numCount - 1 ) {
+           int j = i + 1;
+           while( j < numCount - 1 ) {
+                int complement = -( nums[ i ] + nums[ j ] );       
+
+                // Elements less than j + 1 have already been searched
+                int k = binarySearch( nums, j + 1, numCount - 1, complement );
+                if( k != -1 ) {
+                    result.push_back( vector<int>{ nums[i], nums[j], nums[k] } );       
+                }
+                j = scanToUniqueValue( nums, j );
+           }
+           i = scanToUniqueValue( nums, i );
         }
-
-        // All positive or negative. Can't have three 0's
-        if( i == 0 || i == nums.size() ) {
-            return result;
-        }
-
-        int left = i - 1;
-        int middel = i;
-        int right = i + 1;
-
         return result;
     }
 };
